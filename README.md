77eb8314954e387a22caeb2fab714c5a3d0ec5f1
Brian Scramlin <bscramlin@campusedu.com>
2022-05-06 10:17:20 -0400

    Write ext

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Better DDG Image Search
+
+DuckDuckGo images are too small on their search gallery. This makes them bigger.
---
+++ betterGallery.js
@@ -0,0 +1,35 @@
+const styles = document.createElement('style');
+styles.innerHTML = `
+    .detail.detail--slider.detail--images.detail--xd {
+        position: fixed !important;
+        top: 0 !important;
+        left: 0 !important;
+        z-index: 5 !important;
+        height: 100vh !important;
+        padding: 2rem !important;
+    }
+
+    .detail__pane {
+        height: 100% !important;
+    }
+
+    .detail__body.detail__body--images {
+        height: 100% !important;
+        display: flex !important;
+        justify-content: center !important;
+        align-items: center !important;
+    }
+
+    .detail__media__img-link.js-detail-img.js-image-detail-link,
+    .detail__media__img-highres.js-detail-img.js-detail-img-high,
+    .detail__media__img-thumbnail.js-detail-img.js-detail-img-thumb
+    {
+        height: auto !important;
+        width: 100% !important;
+    }
+
+    .detail__close.js-detail-close {
+        top: 5rem !important;
+    }
+`;
+document.head.appendChild(styles);
